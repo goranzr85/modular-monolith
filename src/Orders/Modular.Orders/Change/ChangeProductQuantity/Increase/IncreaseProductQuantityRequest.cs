283d699c7a40b2f13039818db6feb7b119b5f9ab
goranzr85 <nikolicgoranzr85@gmail.com>
2025-04-04 13:55:23 +0200

    supported increasing products in orders

---
+++ src/Orders/Modular.Orders/Change/ChangeProductQuantity/Increase/IncreaseProductQuantityRequest.cs
@@ -0,0 +1,4 @@
+﻿using Modular.Common;
+
+namespace Modular.Orders.Change.ChangeProductQuantity.Increase;
+internal sealed record IncreaseProductQuantityRequest(int ProductId, uint Quantity) : IDomainEvent;
