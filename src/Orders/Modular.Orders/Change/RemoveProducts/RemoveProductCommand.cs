d0c70bae344802180a1743f8a63329f31de2787e
goranzr85 <nikolicgoranzr85@gmail.com>
2025-04-04 13:57:03 +0200

    supported removing products from orders

--- src/Orders/Modular.Orders/Change/RemoveProducts/RemoveProductCommand.cs
+++ src/Orders/Modular.Orders/Change/RemoveProducts/RemoveProductCommand.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Modular.Orders.Errors;
 using Modular.Orders.Models;
 
@@ -10,14 +11,19 @@ internal sealed record RemoveProductCommand(Guid OrderId, int ProductId) : IRequ
 internal sealed class RemoveProductCommandHandler : IRequestHandler<RemoveProductCommand, ErrorOr<Unit>>
 {
     private readonly OrderDbContext _orderDbContext;
+    private readonly ILogger<RemoveProductCommandHandler> _logger;
 
-    public RemoveProductCommandHandler(OrderDbContext orderDbContext)
+    public RemoveProductCommandHandler(OrderDbContext orderDbContext, ILogger<RemoveProductCommandHandler> logger)
     {
         _orderDbContext = orderDbContext;
+        _logger = logger;
     }
 
     public async Task<ErrorOr<Unit>> Handle(RemoveProductCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Removing product from order {OrderId} and product {ProductId}.",
+            request.OrderId, request.ProductId);
+
         Order? order = await _orderDbContext.Orders
             .Include(x => x.Items)
             .FirstOrDefaultAsync(x => x.Id == request.OrderId, cancellationToken);
@@ -31,6 +37,8 @@ internal sealed class RemoveProductCommandHandler : IRequestHandler<RemoveProduc
 
         _orderDbContext.Update(order);
         await _orderDbContext.SaveChangesAsync(cancellationToken);
+
+        _logger.LogInformation("Product {ProductId} removed from order {OrderId}.", request.ProductId, request.OrderId);
 
         return Unit.Value;
     }
