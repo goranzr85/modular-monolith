ed7b1cc92b6bc06d05fddeddf96209c64f580546
goranzr85 <nikolicgoranzr85@gmail.com>
2025-04-04 13:56:46 +0200

    supported adding products to orders

--- src/Orders/Modular.Orders/Change/AddProducts/AddProductCommand.cs
+++ src/Orders/Modular.Orders/Change/AddProducts/AddProductCommand.cs
@@ -1,6 +1,8 @@
 ﻿using ErrorOr;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 using Modular.Common;
 using Modular.Orders.Errors;
 using Modular.Orders.Models;
@@ -11,16 +13,22 @@ internal sealed record AddProductCommand(Guid OrderId, int ProductId, uint Quant
 internal sealed class AddProductCommandHandler : IRequestHandler<AddProductCommand, ErrorOr<Unit>>
 {
     private readonly OrderDbContext _orderDbContext;
-    
-    public AddProductCommandHandler(OrderDbContext orderDbContext)
+    private readonly ILogger<AddProductCommandHandler> _logger;
+
+    public AddProductCommandHandler(OrderDbContext orderDbContext, ILogger<AddProductCommandHandler> logger)
     {
         _orderDbContext = orderDbContext;
+        _logger = logger;
     }
 
     public async Task<ErrorOr<Unit>> Handle(AddProductCommand request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Adding product to order {OrderId} and product {ProductId} by {Quantity}.",
+            request.OrderId, request.ProductId, request.Quantity);
+
         Order? order = await _orderDbContext.Orders
             .Include(x => x.Items)
+            .AsNoTracking()
             .FirstOrDefaultAsync(x => x.Id == request.OrderId, cancellationToken);
         
         if (order is null)
@@ -28,19 +36,47 @@ internal sealed class AddProductCommandHandler : IRequestHandler<AddProductComma
             return OrderErrors.OrderNotFound(request.OrderId);
         }
 
-        Product? product = await _orderDbContext.Products.FirstOrDefaultAsync(x => x.Id == request.ProductId, cancellationToken);
+        using var transaction = await _orderDbContext.Database.BeginTransactionAsync(System.Data.IsolationLevel.ReadCommitted, cancellationToken);
 
-        if (product is null)
+        try
         {
-            return OrderErrors.ProductNotFound(request.ProductId);
+            var product = await _orderDbContext.Products
+              .FromSqlRaw("SELECT StockQuantity FROM Products WHERE Id = {0} FOR UPDATE", request.ProductId)
+              .Select(p => new { p.StockQuantity })
+              .FirstOrDefaultAsync(cancellationToken);
+
+            if (product is null)
+            {
+                return OrderErrors.ProductNotFound(request.ProductId);
+            }
+
+            if (product.StockQuantity < request.Quantity)
+            {
+                return OrderErrors.ProductQuantityIsNotEnough(request.ProductId);
+            }
+
+            order.AddItem(request.ProductId, request.Quantity, request.Price);
+
+            _orderDbContext.Update(order);
+            await _orderDbContext.SaveChangesAsync(cancellationToken);
+
+            await transaction.CommitAsync(cancellationToken);
+
+            _logger.LogInformation("Product added to order {OrderId} and product {ProductId} by {Quantity}.",
+                request.OrderId, request.ProductId, request.Quantity);
+            
+            return Unit.Value;
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            
+            _logger.LogError(ex, "An error occurred while adding product to order {OrderId} and product {ProductId} by {Quantity}.",
+                request.OrderId, request.ProductId, request.Quantity);
+            
+            return OrderErrors.AddItemToOrderError(order.Id, request.ProductId);
         }
 
-        order.AddItem(request.ProductId, request.Quantity, request.Price);
-
-        _orderDbContext.Update(order);
-        await _orderDbContext.SaveChangesAsync(cancellationToken);
-        
-        return Unit.Value;
     }
 }
 
