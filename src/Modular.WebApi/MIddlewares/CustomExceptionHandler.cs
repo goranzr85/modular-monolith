d3e4a25d924b889169e1ac7ed8fa98fe8a4b20b5
goranzr85 <nikolicgoranzr85@gmail.com>
2025-04-04 13:50:01 +0200

    adding exception handler middleware

---
+++ src/Modular.WebApi/MIddlewares/CustomExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modular.WebApi.MIddlewares;
+
+public sealed class CustomExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<CustomExceptionHandler> _logger;
+
+    public CustomExceptionHandler(ILogger<CustomExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occured: {Message}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server Error",
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1"
+        };
+
+        httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
