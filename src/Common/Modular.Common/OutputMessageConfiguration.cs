0f6556c2f2b750a6dc7f3a5db3216ada2b249ecd
goranzr85 <nikolicgoranzr85@gmail.com>
2025-10-06 14:17:14 +0200

    supported outbox message pattern as common part for event architecture

---
+++ src/Common/Modular.Common/OutputMessageConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Modular.Common;
+public class OutputMessageConfiguration : IEntityTypeConfiguration<OutboxMessage>
+{
+    public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Type)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(c => c.Content)
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder.Property(c => c.OccurredOnUtc)
+            .IsRequired();
+
+        builder.Property(c => c.Error)
+            .HasMaxLength(3000);
+
+        builder.ToTable("OutboxMessages");
+    }
+}
--- src/Common/Modular.Common/User/Configuration/ContactConfiguration.cs
+++ src/Common/Modular.Common/User/Configuration/ContactConfiguration.cs
@@ -4,5 +4,6 @@ public static class ContactConfiguration
 {
     public static byte EmailMaxLength => 80;
     public static byte PhoneMaxLength => 50;
+    public static byte PrimaryContactTypeMaxLength => 20;
 
 }
