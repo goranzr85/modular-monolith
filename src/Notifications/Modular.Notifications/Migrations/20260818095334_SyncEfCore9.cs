49e50048e50b173284bb5db8237e4c1d74fac851
Goran <nikolicgoranzr85@gmail.com>
2026-08-18 12:47:40 +0200

    updated to .net 9

---
+++ src/Notifications/Modular.Notifications/Migrations/20260818095334_SyncEfCore9.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modular.Notifications.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncEfCore9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
